121601a3d9e6a63ad57d2671c0bec072515e9aea
BekhzodRasulov <bekhzodr2001@gmail.com>
2024-09-27 00:09:57 +0500

    JS math

--- app.js
+++ app.js
@@ -1,2 +1,235 @@
 // Boolean
 // 1 misol
+// A butun son berilgan. Jumlani rostligini tekshiring: "A soni musbat".
+// const num1 = prompt("Raqam kiriting:");
+// if (num1 > 0) {
+//     alert("Kiritilgan son musbat");
+//     console.log("Kiritilgan son musbat");
+// } else {
+//     alert("Musbat son emas");
+// }
+
+// 2 misol
+// A butun son berilgan. Jumlani rostligini tekshiring: "A soni toq son".
+// const num1 = prompt("Raqam kiriting:");
+// if (num1 % 2 == 1) {
+//     alert("Kiritilgan son toq son");
+//     console.log("Kiritilgan son toq son");
+// } else {
+//     alert("Kiritilgan son juft son");
+// }
+
+// 3misol
+// A butun son berilgan. Jumlani rostligini tekshiring: "A soni juft son".
+// const num1 = prompt("Raqam kiriting:");
+// if (num1 % 2 == 0) {
+//     alert("Kiritilgan son juft son");
+//     console.log("Kiritilgan son juft son");
+// } else {
+//     alert("Kiritilgan son toq son");
+// }
+
+// 4 misol
+// A va B butun sonlari berilgan. Jumlani rostligini tekshirin: "A>2 va B<=3".
+// const num1 = prompt("Birinchi raqamni kiriting");
+// const num2 = prompt("Ikkinchi sonni kiriting");
+// if (num1 > 2 && num2 <= 3) {
+//     alert("True");
+// } else {
+//     alert("False");
+// }
+
+// 5 misol
+//A va B butun sonlari berilgan. Jumlani rostligini tekshirin: "A>=0 va B<-2".
+// const num1 = prompt("Birinchi raqamni kiritng");
+// const num2 = prompt("Ikkinchi raqamni kiriting");
+// if (num1 >= 0 || num2 < -2) {
+//     alert("True");
+// } else {
+//     alert("False");
+// }
+
+// 6 misol
+// Uchta A, B va C butun sonlari berilgan. Jumlani rostlogini tekshirin: "A<=B<=C".
+// const num1 = prompt("Birinchi sonni kiriting:");
+// const num2 = prompt("Ikkinchi sonni kiriting:");
+// const num3 = prompt("Uchunchi sonni kiriting:");
+// if (num1 <= num2 && num2 <= num3) {
+//     alert("True");
+// } else {
+//     alert("False");
+// }
+
+// 7 misol
+// Uchta A, B va C butun sonlari berilgan. Jumlani rostlogini tekshirin: "B soni A va C sonlari orasida yotadi".
+// const num1 = prompt("Birinchi sonni kiriting:");
+// const num2 = prompt("Ikkinchi sonni kiriting:");
+// const num3 = prompt("Uchunchi sonni kiriting:");
+// if (num1 <= num2 && num2 <= num3) {
+//     alert("True");
+// } else {
+//     alert("False");
+// }
+
+// 8 misol
+//A va B butun sonlari berilgan. Jumlani rostligini tekshirin: "A va B sonlari toq sonlar".
+// const num1 = prompt("Birinchi sonni kiriting:");
+// const num2 = prompt("Ikkinchi sonni kiriting:");
+// if (num1 % 2 == 1 && num2 % 2 == 1) {
+//     alert("Kiritilgan sonlar toq sonlar");
+// } else {
+//     alert("False");
+// }
+
+// 9 misol
+// A va B butun sonlari berilgan. Jumlani rostligini tekshirin: "A va B sonlari hech bolmaganda bitasi toq son".
+// const num1 = prompt("Birinchi sonni kiriting:");
+// const num2 = prompt("Ikkinchi sonni kiriting:");
+// if (num1 % 2 !== 0 || num2 % 2 !== 0) {
+//     alert("True");
+// } else {
+//     alert("False");
+// }
+
+// 10 misol
+// A va B butun sonlari berilgan. Jumlani rostligini tekshirin: "A va B sonlarining faqat bittasi toq son".
+// const num1 = prompt("Birinchi sonni kiriting:");
+// const num2 = prompt("Ikkinchi sonni kiriting:");
+// if (num1 % 2 == 1 || num2 % 2 == 1) {
+//     alert("Kiritilgan sonlarning biri toq son");
+// } else {
+//     alert("False");
+// }
+
+// 11 misol
+// // A va B butun sonlari berilgan. Jumlani rostligini tekshirin: "A va B sonnalrining har ikkalasi ham yoki toq yoki juft son".
+// const num1 = prompt("Birinchi sonni kiriting:");
+// const num2 = prompt("Ikkinchi sonni kiriting:");
+// if (num1 % 2 === num2 % 2) {
+//     alert("True");
+// } else {
+//     alert("False");
+// }
+
+// 12 misol
+// Uchta A, B va C butun sonlari berilgan. Jumlani rostlogini tekshirin: "A, B, C sonlarning har biri musbat".
+// const num1 = prompt("Birinchi sonni kiriting:");
+// const num2 = prompt("Ikkinchi sonni kiriting:");
+// const num3 = prompt("Uchunchi sonni kiriting:");
+// if (num1 > 0 && num2 > 0 && num3 > 0) {
+//     alert("Baracha sonlar musbat");
+// } else {
+//     alert("False");
+// }
+
+// 13 misol
+// Uchta A, B va C butun sonlari berilgan. Jumlani rostlogini tekshirin: "A, B, C sonlarning hech bo'lmaganda bittasi musbat".
+// const num1 = prompt("Birinchi sonni kiriting:");
+// const num2 = prompt("Ikkinchi sonni kiriting:");
+// const num3 = prompt("Uchunchi sonni kiriting:");
+// if (num1 > 0 || num2 > 0 || num3 > 0) {
+//     alert("True");
+// } else {
+//     alert("False");
+// }
+
+// 14 misol
+// Uchta A, B va C butun sonlari berilgan. Jumlani rostlogini tekshirin: "A, B, C sonlarning faqat bittasi musbat".
+// const num1 = prompt("Birinchi sonni kiriting:");
+// const num2 = prompt("Ikkinchi sonni kiriting:");
+// const num3 = prompt("Uchunchi sonni kiriting:");
+// if (
+//     (num1 > 0 && num2 < 0 && num3 < 0) ||
+//     (num1 < 0 && num2 > 0 && num3 < 0) ||
+//     (num1 < 0 && num2 < 0 && num3 > 0)
+// ) {
+//     alert("True");
+// } else {
+//     alert("False");
+// }
+
+// 15 misol
+// Uchta A, B va C butun sonlari berilgan. Jumlani rostlogini tekshirin: "A, B, C sonlarning faqat ikktasi musbat".
+// const num1 = prompt("Birinchi sonni kiriting:");
+// const num2 = prompt("Ikkinchi sonni kiriting:");
+// const num3 = prompt("Uchunchi sonni kiriting:");
+// if (
+//     (num1 > 0 && num2 > 0 && num3 < 0) ||
+//     (num1 < 0 && num2 > 0 && num3 > 0) ||
+//     (num1 > 0 && num2 < 0 && num3 > 0)
+// ) {
+//     alert("True");
+// } else {
+//     alert("False");
+// }
+
+// If
+// 1 misol
+// Butun son berilgan.Agar, berilgan son musbat bo'lsa, 1ga oshirilsin, aks holda o'zgartirilmasin. Hosil bo'lgan sonni ekranga chiqaruvchi programma tuzilsin.
+// const num1 = prompt("Raqam kiriting:");
+// if (num1 > 0) {
+//     alert(Number(num1) + Number(1));
+// } else {
+//     alert("False");
+// }
+
+// 2 misol
+// const num1 = prompt("Raqam kiriting:");
+// if (num1 > 0) {
+//     alert(Number(num1) + Number(1));
+// } else {
+//     alert(Number(num1) - Number(2));
+// }
+
+// 3 misol
+// const num1 = prompt("Raqam kiriting:");
+// if (num1 > 0) {
+//     alert(Number(num1) + Number(1));
+// } else if (num1 < 0) {
+//     alert(Number(num1) - Number(2));
+// } else if (num1 == 0) {
+//     alert(Number(10));
+// } else {
+//     alert("False");
+// }
+
+// 4 misol
+// Uchta son berilgan. Shu sonlar orasidan nechta musbat son borligini aniqlovchi programma tuzilsin.
+// const num1 = prompt("Birinchi sonni kiriting:");
+// const num2 = prompt("Ikkinchi sonni kiriting:");
+// const num3 = prompt("Uchunchi sonni kiriting:");
+// let result = 0;
+// if (num1 > 0) {
+//     result = result + 1;
+// }
+// if (num2 > 0) {
+//     result = result + 1;
+// }
+// if (num3 > 0) {
+//     result = result + 1;
+// }
+// console.log(" Musbat sonlar soni " + result);
+
+// 5 misol
+// const num1 = prompt("Birinchi sonni kiriting:");
+// const num2 = prompt("Ikkinchi sonni kiriting:");
+// const num3 = prompt("Uchunchi sonni kiriting:");
+// let positive = 0;
+// let negative = 0;
+// if (num1 > 0) {
+//     positive = positive + 1;
+// } else {
+//     negative = negative + 1;
+// }
+// if (num2 > 0) {
+//     positive = positive + 1;
+// } else {
+//     negative = negative + 1;
+// }
+// if (num3 > 0) {
+//     positive = positive + 1;
+// } else {
+//     negative = negative + 1;
+// }
+// console.log("Musbat sonlar soni " + positive);
+// console.log("Manfiy sonlar soni " + negative);
